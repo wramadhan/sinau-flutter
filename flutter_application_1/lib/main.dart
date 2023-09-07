e486b473a94073f172447014ceda092b680d2c4c
wahyu.ramadhan <superwahyoe@gmail.com>
2023-09-07 12:37:34 +0700

    add : invisible widget

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
+main() {
   runApp(MyApp());
 }
 
@@ -10,31 +10,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: Color(0xff545454),
-          // backgroundColor: Colors.green,
           appBar: AppBar(
-            title: Text('My App 2'),
+            title: Text('Column'),
             centerTitle: true,
           ),
-          body: Center(
-            child: Text(
-              'ini text asdsadsadsadsadasdasdahsaddljasdsadasbdadkjahsdkjahsdkjahsdajsadsasdahdshkjasdkjaskdjhkasdlkjhasldhjasdlkjhasdlkjhasdlkjhasdlkjhasdkljh',
-              // maxLines: 2,
-              // overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  backgroundColor: Colors.amber,
-                  color: Colors.white,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 10,
-                  fontFamily: 'Poppins',
-                  decoration: TextDecoration.lineThrough,
-                  // decoration: TextDecoration.underline,
-                  decorationStyle: TextDecorationStyle.wavy,
-                  decorationColor: Colors.white
-                  decorationThickness: 2),
-            ),
+          // body: Row( //bisa Row, Column, dan Stack
+          body: Stack(
+            // mainAxisAlignment: MainAxisAlignment.center, //hanya bisa untuk column dan row
+            // crossAxisAlignment: CrossAxisAlignment.end,//hanya bisa untuk column dan row
+            children: [
+              Container(
+                width: 400,
+                height: 400,
+                color: Colors.green,
+              ),
+              Container(
+                width: 200,
+                height: 150,
+                color: Colors.blue[400],
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.amber,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.blue,
+              ),
+            ],
           )),
     );
   }
